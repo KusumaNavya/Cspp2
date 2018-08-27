63730a479d15ea7f709af585a55b6db6fe6226fb
KusumaNavya <navya.kusuma21@msitprogram.net>
2018-08-27 18:39:23 +0530

    Hello

---
+++ cspp2-practice/m1/a3/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+*helloworld.
+* @author Navya.
+*/
+public final class HelloWorld {
+   /**
+    * empty constructor.
+    */
+   private HelloWorld() {
+}
+/**
+ *main functio to print helloworld.
+  *@param args is the parameter for the statement.
+*/
+    public static void main(final String[] args) {
+        System.out.println("Hello Navya");
+    }
+}
