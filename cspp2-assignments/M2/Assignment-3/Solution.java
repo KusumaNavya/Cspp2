5d8b01881dd01b16f9fd981dc593659d5dc7692c
KusumaNavya <navya.kusuma21@msitprogram.net>
2018-08-28 18:14:52 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/M2/Assignment-3/Solution.java
+++ cspp2-assignments/M2/Assignment-3/Solution.java
@@ -15,14 +15,14 @@ public final class Solution {
         Scanner s = new Scanner(System.in);
         int base = s.nextInt();
         int exponent = s.nextInt();
-        double result = power(base,exponent);
-        System.out.println((long)result);
+        double result = power(base, exponent);
+        System.out.println((long) result);
     }
     /**
     @param base Need to write the power function and print the output.
     @param exponent Need to write the power function and print the output.
-    */
-    public static double power(final int base, final int exponent){
+    @return double**/
+    public static double power(final int base, final int exponent) {
         return Math.pow(base, exponent);
     }
 }
