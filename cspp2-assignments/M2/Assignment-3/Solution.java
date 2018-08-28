8f0030a50f3ce37229325a7eb331936c65df46aa
KusumaNavya <navya.kusuma21@msitprogram.net>
2018-08-28 18:11:07 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/M2/Assignment-3/Solution.java
+++ cspp2-assignments/M2/Assignment-3/Solution.java
@@ -1,25 +1,28 @@
 import java.util.Scanner;
-
-public class Solution {
-	 /**
+ /**
     *
     **/
-   	private Solution() {
-	}
-	/*
-	Do not modify this main function.
-	*/
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);      
+public final class Solution {
+     /**
+    *
+    **/
+    private Solution() {
+    }
+    /**
+    @param argsDo not modify this main function.
+    */
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
         int base = s.nextInt();
         int exponent = s.nextInt();
         double result = power(base,exponent);
         System.out.println((long)result);
-	}
-	/*
-	Need to write the power function and print the output.
-	*/
-	public static double power(int base, int exponent){
-		return Math.pow(base, exponent);
-	}
+    }
+    /**
+    @param base Need to write the power function and print the output.
+    @param exponent Need to write the power function and print the output.
+    */
+    public static double power(final int base, final int exponent){
+        return Math.pow(base, exponent);
+    }
 }
