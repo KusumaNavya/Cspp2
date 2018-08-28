cc5c67c718ae34fad78e4927d3aa2da3c18a6a83
KusumaNavya <navya.kusuma21@msitprogram.net>
2018-08-28 09:33:51 +0530

    A1

---
+++ cspp2-assignments/M1/Assignment-1/Solution.java
@@ -0,0 +1,22 @@
+/**
+*
+*
+*/
+import java.util.*;
+public final class Solution {
+   /**
+    *
+    */
+   private Solution() {
+}
+/**
+ *main functio.
+  *@param args is the parameter for the statement.
+*/
+    public static void main(final String[] args) {
+       Scanner sc = new Scanner(System.in);
+        String name = sc. nextLine();
+        int rollnumber = sc.nextInt();
+        System.out.println(name +","+rollnumber);
+    }
+}
