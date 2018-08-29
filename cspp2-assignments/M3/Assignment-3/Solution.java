24eedf7e9c5b6ba99c42db59ede093f99d088abf
KusumaNavya <navya.kusuma21@msitprogram.net>
2018-08-29 15:44:46 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/M3/Assignment-3/Solution.java
+++ cspp2-assignments/M3/Assignment-3/Solution.java
@@ -1,14 +1,14 @@
 
 import java.util.Scanner;
 /*
-	Do not modify this main function.
-	*/
+    Do not modify this main function.
+    */
 public final class Solution {
     public static void main(final String[] args) {
-        Scanner s=new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
         int n1 = s.nextInt();
         int n2 = s.nextInt();
-        int gcd = gcd(n1,n2);
+        int gcd = gcd(n1, n2);
         System.out.println(gcd);
     }
     /**
@@ -16,7 +16,6 @@ public final class Solution {
     
     @param      n1    The n 1
     @param      n2    The n 2
-    
     @return     { description_of_the_return_value }
     */
     public static int gcd(int n1, int n2) {
@@ -26,7 +25,7 @@ public final class Solution {
         } else {
             min = n2;
         }
-        for (int i = min;i > 1 ;i-- ) {
+        for (int i = min; i > 1 ; i--) {
             if(n1 % i == 0 && n2 % i == 0) {
                 return i;
             }
