2895002b7f7571cc371cfc6e1febb9099204e606
KusumaNavya <navya.kusuma21@msitprogram.net>
2018-09-01 11:13:37 +0530

    "check_style_score -> 0 of 5 passed. style score: 0/1 "

--- cspp2-assignments/M6/Assignment-2/Solution.java
+++ cspp2-assignments/M6/Assignment-2/Solution.java
@@ -24,41 +24,41 @@ final class Solution {
      */
     static int[][] roundHundred(final int[][] a, final int rows, final int columns) {
 // write your code here
-            for (int i = 0; i <= rows; i++) {
-        for (int j = 0; j < columns; j++) {
-            if (a[i][j] >= 0 || a[i][j] <= 50) {
-                a[i][j] = 0;
-            }
-            else if (a[i][j] >= 50 || a[i][j] <= 150) {
-                a[i][j] = 100;
-            }
-            else if (a[i][j] >= 150 || a[i][j] <= 250) {
-                a[i][j] = 200;
-            }
-            else if (a[i][j] >= 250 || a[i][j] <= 350) {
-                a[i][j] = 300;
-            }
-            else if (a[i][j] >= 350 || a[i][j] <= 450) {
-                a[i][j] = 400;
-            }
-            else if (a[i][j] >= 450 || a[i][j] <= 550) {
-                a[i][j] = 500;
-            }
-            else if (a[i][j] >= 550 || a[i][j] <= 650) {
-                a[i][j] = 600;
-            }
-            else if (a[i][j] >= 650 || a[i][j] <= 750) {
-                a[i][j] = 700;
-            }
-            else if (a[i][j] >= 750 || a[i][j] <= 850) {
-                a[i][j] = 800;
-            }
-            else if (a[i][j] >= 850 || a[i][j] <= 950) {
-                a[i][j] = 900;
-            }
-            else if (a[i][j] >= 950 || a[i][j] <= 1050) {
-                a[i][j] = 1000;
-            }
+        for (int i = 0; i <= a.length; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (a[i][j] >= 0 || a[i][j] <= 50) {
+                    a[i][j] = 0;
+                 }
+                else if (a[i][j] >= 50 || a[i][j] <= 150) {
+                    a[i][j] = 100;
+                }
+                else if (a[i][j] >= 150 || a[i][j] <= 250) {
+                    a[i][j] = 200;
+                }
+                else if (a[i][j] >= 250 || a[i][j] <= 350) {
+                    a[i][j] = 300;
+                }
+                else if (a[i][j] >= 350 || a[i][j] <= 450) {
+                    a[i][j] = 400;
+                }
+                else if (a[i][j] >= 450 || a[i][j] <= 550) {
+                    a[i][j] = 500;
+                }
+                else if (a[i][j] >= 550 || a[i][j] <= 650) {
+                    a[i][j] = 600;
+                }
+                else if (a[i][j] >= 650 || a[i][j] <= 750) {
+                    a[i][j] = 700;
+                }
+                else if (a[i][j] >= 750 || a[i][j] <= 850) {
+                    a[i][j] = 800;
+                }
+                else if (a[i][j] >= 850 || a[i][j] <= 950) {
+                    a[i][j] = 900;
+                }
+                else if (a[i][j] >= 950 || a[i][j] <= 1050) {
+                    a[i][j] = 1000;
+                }
         }
     }  return (a);     
     }
