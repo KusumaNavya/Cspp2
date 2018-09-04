4076a70d07e76608e06f119fe136d6cd0a93ac21
KusumaNavya <navya.kusuma21@msitprogram.net>
2018-09-04 10:42:49 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ cspp2-assignments/M7/Assignment-1/Solution.java
@@ -0,0 +1,53 @@
+/**
+  * 
+*/
+import java.util.Scanner;
+/**
+ * Class for input validator.
+ */
+class InputValidator {
+  /*Write the atrributes and methods for InputValidator*/
+  /**
+   * input string.
+   */
+  private String inpData;
+  /**
+   * Constructs the object.
+   *
+   * @param      iD    input string
+   */
+  InputValidator(final String iD) {
+    inpData = iD;
+  }
+  /**
+   * Validates data.
+   *
+   * @return     boolean for validating the input data.
+   */
+  boolean validateData() {
+    final int lenCheck = 6;
+    return inpData.length() >= lenCheck;
+  }
+}
+/**
+ * Class for solution.
+ */
+public final class Solution {
+  /**
+   * Constructs the object.
+   */
+  private Solution() {
+    //unused
+  }
+  /**
+  * The main function.
+  *
+  * @param  args  The arguments
+  */
+  public static void main(final String[] args) {
+    Scanner s = new Scanner(System.in);
+    String input = s.next();
+    InputValidator i = new InputValidator(input);
+    System.out.println(i.validateData());
+  }
+}
