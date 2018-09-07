9c45fd106cb151ac441b750ad331c4e0fd8880c1
KusumaNavya <navya.kusuma21@msitprogram.net>
2018-09-07 14:08:31 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- cspp2-assignments/M11/Assignment-1/List.java
+++ cspp2-assignments/M11/Assignment-1/List.java
@@ -259,10 +259,10 @@ public class List {
         //created a new object with name 'l' and assigned the sublist to 'l'.
         List l = new List();
         int size = 0;
-        if (start < 0 && end < 0 && start > end) {
+        if (start < 0 || end < 0 || start > end) {
             System.out.println("Index Out of Bounds Exception");
         } else {
-            for (int i = start; i < end; i++) {
+            for (int i = start; i <= end; i++) {
                 l.add(list[i]);
                 size++;
             }
