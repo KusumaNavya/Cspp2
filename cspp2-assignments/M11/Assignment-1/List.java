710bda2602c533ea76b4436bd2485bbbdcf22113
KusumaNavya <navya.kusuma21@msitprogram.net>
2018-09-07 18:12:52 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- cspp2-assignments/M11/Assignment-1/List.java
+++ cspp2-assignments/M11/Assignment-1/List.java
@@ -197,7 +197,7 @@ public class List {
         }
         String str = "[";
         int i = 0;
-        for (i = 0; i < size - 1; i++) {
+        for (i = 0; i < size; i++) {
             str = str + list[i] + ",";
         }
         str = str + list[i] + "]";
