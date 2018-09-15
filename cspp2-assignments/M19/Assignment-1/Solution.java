13685184ccf6e0341de7398e27d5a80983e313fb
KusumaNavya <navya.kusuma21@msitprogram.net>
2018-09-15 11:17:37 +0530

    "check_style_score -> 0 of 13 passed. style score: 0/1 "

--- cspp2-assignments/M19/Assignment-1/Solution.java
+++ cspp2-assignments/M19/Assignment-1/Solution.java
@@ -8,7 +8,6 @@ class Quiz {
 
     Quiz(final int quizz) {
         this.quiz = quizz;
-        System.out.println(quiz);
     }
     public int getquiz() {
         return quiz;
@@ -97,7 +96,9 @@ public final class Solution {
         // write your code here to display the quiz questions
         // read the user responses from the console
         // store the user respones in the quiz object
-        //for (int i = 0; i <= quiz; i++)
+        for (int i = 0; i <= answerCount; i++) {
+            System.out.println("question text " + quiz + "(" + s + ")");
+        }
     }
 
     /**
