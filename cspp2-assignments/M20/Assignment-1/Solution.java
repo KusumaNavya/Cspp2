f4ab0fe4dd6a429cc1fdece4a2f41a380210be58
KusumaNavya <navya.kusuma21@msitprogram.net>
2018-09-17 11:59:05 +0530

    "check_style_score -> 0 of 13 passed. style score: 0/1 "

--- cspp2-assignments/M20/Assignment-1/Solution.java
+++ cspp2-assignments/M20/Assignment-1/Solution.java
@@ -243,8 +243,7 @@ public final class Solution {
         // tokenize the question line and create the question object
         // add the question objects to the quiz class
         for (int i = 0; i <= q; i++ ) {
-        String line = scan.nextLine();
-        String[] tokens = line.split(":");
+        String[] tokens = scan.nextLine().split(":");
         String[] choice = tokens[1].split(",");
         Question ques = new Question(tokens[0], choice, Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3]), Integer.parseInt(tokens[4]));
         if(tokens[0].equals("") || tokens[1].equals("") || tokens[2].equals("") || tokens[3].equals("") || tokens[4].equals("")) {
