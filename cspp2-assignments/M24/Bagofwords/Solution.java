ba3e09bae9bb1c894c9eae9e2fe1430d40dc7d0f
KusumaNavya <navya.kusuma21@msitprogram.net>
2018-09-23 23:22:45 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- cspp2-assignments/M24/Bagofwords/Solution.java
+++ cspp2-assignments/M24/Bagofwords/Solution.java
@@ -1,115 +1,169 @@
-import java.io.*;
-import java.util.*;
-class Frequency {
-    Frequency() {
-
+/**
+ * 
+ */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
     }
-    public Hashtable<String,Integer> loadWordstodict (File f)
-    throws Exception {
-        FileInputStream fileinp = new FileInputStream(f);
-        BufferedReader reader = new BufferedReader(
-            new InputStreamReader(fileinp));
-        String str;
-        Hashtable<String,Integer> dictionary = new Hashtable<String,Integer>();
-        try {
-            int c = 0;
-            while ((str = reader.readLine()) != null) {
-                // String s = str.replaceAll("[;:.,\"\\[\\]/!@#$%^&*()]","");
-                String s = str.replaceAll("[^a-z A-Z 0-9 _]","").toLowerCase();
-                String[] line = s.split(" ");
-                for (int i = 0; i < line.length; i++) {
-                    if (dictionary.containsKey(line[i])) {
-                        dictionary.put(line[i], dictionary.get(line[i]) + 1);
-                    } else {
-                        dictionary.put(line[i], 1);
+    /**
+     * variable declaration.
+     */
+    private static final int THIRTEEN = 13;
+    /**
+     * variable declaration.
+     */
+    private static final int HUNDRED = 100;
+    /**
+     * variable declaration.
+     */
+    private static final double TWOHUNDRED = 200.0;
+    /**
+     * to calculate lcs.
+     *
+     * @param      doc1  The document 1
+     * @param      doc2  The document 2
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static int lcs(final String doc1, final String doc2) {
+        int lcsmax = 0, lcs = 0, temp = 0;
+        for (int i = 0; i < doc1.length() - 1; i++) {
+            int j = 0;
+            while (j < doc2.length() - 1) {
+                temp = i;
+                lcs = 0;
+                if (doc1.charAt(temp) == (doc2.charAt(j))
+                        && doc1.charAt(temp) != ' ') {
+                    while (doc1.charAt(temp) == (doc2.charAt(j)) && j
+                            < doc2.length() - 1 && temp < doc1.length() - 1) {
+                        lcs++;
+                        j++;
+                        temp++;
                     }
-                c++;
+                    if (lcs > lcsmax) {
+                        lcsmax = lcs;
+                    }
+                } else {
+                    j++;
                 }
             }
-            // System.out.println("Words loaded" + c);
+        }
+        return lcsmax + 1;
+    }
+    /**
+     * to print result in given format.
+     *
+     * @param      matchpercentmat  The matchpercentmat
+     * @param      filelist         The filelist
+     */
+    public static void printResult(final float[][] matchpercentmat,
+                                   final File[] filelist) {
+        String[] fileListAsString = new String[filelist.length];
+        for (int i = 0; i < filelist.length; i++) {
+            for (int j = 0; i < filelist[i].toString().length(); j++) {
+                if (filelist[i].toString().charAt(j) == '\\') {
+                    fileListAsString[i] = filelist[i].toString()
+                                          .substring(j + 1);
+                    break;
+                }
+            }
+        }
+        String res = "         ";
+        for (String eachFile : fileListAsString) {
+            // res += eachFile + "\t";
+            int numberOfSpaces = THIRTEEN - eachFile.length();
+            for (int spindex = 0; spindex < numberOfSpaces; spindex++) {
+                res += " ";
+            }
+            res += eachFile;
+        } res += " \n";
+        for (int i = 0; i < fileListAsString.length; i++) {
+            res += fileListAsString[i];
+            for (int j = 0; j < fileListAsString.length; j++) {
+                int numberOfSpaces = THIRTEEN - (matchpercentmat[i][j] + "")
+                                     .length();
+                for (int spindex = 0; spindex < numberOfSpaces; spindex++) {
+                    res += " ";
+                }
+                res += matchpercentmat[i][j] + "";
+            } res += " \n";
+        }
+        System.out.print(res);
+        float maxpercetmatch = 0;
+        String file1 = "", file2 = "";
+        for (int i = 0; i < filelist.length; i++) {
+            for (int j = 0; j < filelist.length; j++) {
+                if (i < j && maxpercetmatch < matchpercentmat[i][j]) {
+                    file1 = fileListAsString[i];
+                    file2 = fileListAsString[j];
+                    maxpercetmatch = matchpercentmat[i][j];
+                }
+            }
+        }
+        System.out.println("Maximum similarity is between "
+                           + file1 + " and " + file2);
+    }
+    /**
+     * main function.
+     *
+     * @param      args       The arguments
+     *
+     * @throws     Exception  { exception_description }
+     */
+    public static void main(final String[] args) throws Exception {
+        try {
+            Scanner scan = new Scanner(System.in);
+            String foldername = scan.next();
+            File folder = new File(foldername);
+            File[] filelist = folder.listFiles();
+            String[] strlist = new String[filelist.length];
+            try {
+                int filecount = 0;
+                for (File file : filelist) {
+                    Scanner filescan = new Scanner(file);
+                    String str = "";
+                    while (filescan.hasNextLine()) {
+                        str += filescan.nextLine() + " ";
+                    }
+                    // System.out.println(str);
+                    strlist[filecount++] = str.trim();
+                }
+            } catch (FileNotFoundException e) {
+                System.out.println("file not found");
+            }
+            float[][] matchpercentmat = new float[filelist.length]
+            [filelist.length];
+            for (int i = 0; i < filelist.length; i++) {
+                for (int j = 0; j < filelist.length; j++) {
+                    if (i == j) {
+                        matchpercentmat[i][j] = HUNDRED;
+                    } else {
+                        int lcsmax = 0;
+                        if (!(strlist[i].equals("") || strlist[j]
+                                .equals(""))) {
+                            if (strlist[i].length() > strlist[j]
+                                    .length()) {
+                                lcsmax = lcs(strlist[i], strlist[j]);
+                            } else {
+                                lcsmax = lcs(strlist[j], strlist[i]);
+                            }
+                        }
+                        matchpercentmat[i][j] = Math.round((lcsmax * TWOHUNDRED)
+                            / (strlist[i].length() + strlist[j].length()));
+                    }
+                }
+            }
+            printResult(matchpercentmat, filelist);
         } catch (Exception e) {
-            throw new Exception("File not found");
+            System.out.println("Empty Directory");
         }
-        return dictionary;
-    }
-}
-
-class Distance {
-    Distance() {
-
-    }
-    public double Euclidean(Hashtable<String,Integer> dict1) {
-        long sum = 0;
-        for(Map.Entry<String, Integer> m: dict1.entrySet()) {
-            // int val = (int)m.getValue();
-            sum += m.getValue()*m.getValue();
-        }
-        double prod = Math.sqrt(sum);
-        return prod;
-    }
-    public double DotProduct(Hashtable<String,Integer> dict1,
-        Hashtable<String,Integer> dict2) {
-        String key;
-        int sum = 0;
-        for (Map.Entry<String, Integer> m: dict1.entrySet()) {
-            key = m.getKey();
-            if (dict2.containsKey(key)) {
-                int val1 = dict1.get(key);
-                int val2 = dict2.get(key);
-                sum += (val1*val2);
-            }
-        }
-        return sum;
-    }
-    public double similarity(Hashtable<String,Integer> dictionary1,
-        Hashtable<String,Integer> dictionary2) {
-        double a = Euclidean(dictionary1);
-        double b = Euclidean(dictionary2);
-        double num = a*b;
-        double deno = DotProduct(dictionary1, dictionary2);
-        double result = deno/num;
-        result = result * 100;
-        return Math.round(result);
-    }
-}
-
-class Solution {
-    Solution () {
-
-    }
-    public static void main(String[] args) throws Exception {
-        Scanner scan = new Scanner(System.in);
-        String foldername = scan.nextLine();
-        // Inpfromfolder i = new Inpfromfolder();
-        final File folder = new File(foldername);
-        // ArrayList<String> filenames= i.listFilesForFolder(folder);
-        // System.out.println(folder);
-        File[] allfiles = folder.listFiles();
-        // System.out.println(Arrays.toString(allfiles));
-        String s = "\t";
-        for (File fil: allfiles) {
-            s += fil.getName() + "\t";
-        }
-        // s = s.trim();
-        // s = s.substring(0, s.length()-4);
-        s += "\n";
-        if (allfiles.length != 0) {
-            // System.out.println("Entered if");
-            for (File file1: allfiles) {
-                s += file1.getName() + "\t";
-                for (File file2: allfiles) {
-                    Frequency f = new Frequency();
-                    Distance d = new Distance();
-                    s += d.similarity(f.loadWordstodict(file1), f.loadWordstodict(file2)) + "" + "\t";
-                }
-                s = s.trim();
-                s += "\n";
-            }
-            System.out.println(s);
-        } else {
-            System.out.println("empty directory");
-        }
-        // f.loadWordstodict(filename1);
-        // f.loadWordstodict(filename2);
     }
 }
